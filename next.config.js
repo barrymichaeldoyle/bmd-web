2217915f1db6696c69dfc527132d07f2eb11e86c
Barry Michael Doyle <barry.doyle@universal.one>
2023-11-03 09:50:44 +0200

    removed taint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    taint: true,
-  },
+  // experimental: {
+  //   taint: true,
+  // },
   images: {
     remotePatterns: [
       {
