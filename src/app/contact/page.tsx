e21fccc000e465dcc700b1923433701383595719
Barry Michael Doyle <barry.doyle@universal.one>
2023-11-07 16:42:28 +0200

    fixed mobile layout problems

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,17 +11,19 @@ export const metadata: Metadata = {
 
 export default function ContactPage() {
   return (
-    <Card className="relative max-w-md w-full mx-auto">
-      <h2 className="text-3xl font-extrabold mb-4 text-gray-800 dark:text-white">
-        Get in touch<span className="ml-3">📬</span>
-      </h2>
-      <p className="text-gray-600 dark:text-gray-400 text-lg mb-6">
-        If you&apos;re looking for a collaborator for your next project, seeking
-        freelance services, or have exciting job opportunities, feel free to get
-        in touch using this form. I&apos;m always interested in exploring
-        compelling projects and roles.
-      </p>
-      <ContactForm />
-    </Card>
+    <div className="container mx-auto">
+      <Card className="mrelative ax-w-lg">
+        <h2 className="text-3xl font-extrabold mb-4 text-gray-800 dark:text-white">
+          Get in touch<span className="ml-3">📬</span>
+        </h2>
+        <p className="text-gray-600 dark:text-gray-400 text-lg mb-6">
+          If you&apos;re looking for a collaborator for your next project,
+          seeking freelance services, or have exciting job opportunities, feel
+          free to get in touch using this form. I&apos;m always interested in
+          exploring compelling projects and roles.
+        </p>
+        <ContactForm />
+      </Card>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,82 +22,72 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <div className="bg-primary shadow-lg dark:bg-gray-900 mt-0 mx-[-32px] py-12 mb-8 lg:mx-4 lg:rounded-lg">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-900 dark:text-gray-100 leading-relaxed">
-          Crafting Future-Ready
-          <br />
-          Frontends 🚀
-        </h1>
-      </div>
+    <div className="container mx-auto">
+      <Card>
+        <div className="text-center">
+          <h2 className="text-xl md:text-2xl font-bold text-gray-900 dark:text-gray-100 mb-4">
+            About Me
+          </h2>
+        </div>
 
-      <div className="container mx-auto px-4">
-        <Card>
-          <div className="text-center">
-            <h2 className="text-xl md:text-2xl font-bold text-gray-900 dark:text-gray-100 mb-4">
-              About Me
-            </h2>
+        <div className="flex flex-col md:flex-row md:items-center">
+          <div className="mb-4 md:mr-6 mx-auto md:mx-0">
+            <Image
+              width={150}
+              height={150}
+              src="/profile.png"
+              alt="Barry Michael Doyle Profile Picture"
+              className="rounded-full border-4 border-primary shadow-md"
+              priority
+            />
           </div>
 
-          <div className="flex flex-col md:flex-row md:items-center">
-            <div className="mb-4 md:mr-6 mx-auto md:mx-0">
-              <Image
-                width={150}
-                height={150}
-                src="/profile.png"
-                alt="Barry Michael Doyle Profile Picture"
-                className="rounded-full border-4 border-primary shadow-md"
-                priority
-              />
-            </div>
-
-            <div className="flex-1">
-              <p className="text-md md:text-lg text-gray-700 dark:text-gray-300 mb-4">
-                Hello! I&apos;m{" "}
-                <span className="text-gray-900 dark:text-white font-semibold">
-                  Barry Michael Doyle
-                </span>
-                , a seasoned{" "}
-                <span className="text-indigo-600 dark:text-indigo-400 font-semibold">
-                  Lead React, React Native, NextJS & TypeScript Engineer
-                </span>{" "}
-                based in Cape Town. At SecuritEase, I spearhead frontend
-                innovations, navigating the evolving JavaScript landscape with a
-                sharp focus on the React ecosystem.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <p className="text-md md:text-lg text-gray-700 dark:text-gray-300">
-              I&apos;m deeply invested in{" "}
-              <span className="text-blue-600 dark:text-blue-400 font-semibold">
-                mentoring, building high-performing teams,
+          <div className="flex-1">
+            <p className="text-md md:text-lg text-gray-700 dark:text-gray-300 mb-4">
+              Hello! I&apos;m{" "}
+              <span className="text-gray-900 dark:text-white font-semibold">
+                Barry Michael Doyle
+              </span>
+              , a seasoned{" "}
+              <span className="text-indigo-600 dark:text-indigo-400 font-semibold">
+                Lead React, React Native, NextJS & TypeScript Engineer
               </span>{" "}
-              and championing inclusive, multicultural workspaces. My career
-              spans across New Zealand, the UK, the USA, and South Africa,
-              fostering a global perspective.
-              <br />
-              <br />
-              Beyond the office, I{" "}
-              <span className="text-green-600 dark:text-green-400 font-semibold">
-                contribute to open source projects, author blog posts,
-              </span>{" "}
-              and engage on StackOverflow. Additionally, I create{" "}
-              <span className="text-purple-600 dark:text-purple-400 font-semibold">
-                educational content
-              </span>{" "}
-              on ReactJS and React Native for YouTube and Udemy. My ambition is
-              to create a lasting, positive impact in the tech world.
-              <br />
-              <br />
-              I&apos;m always open to new challenges and collaborations, eager
-              to connect with fellow tech enthusiasts and innovators. Let’s
-              build groundbreaking digital solutions together!
+              based in Cape Town. At SecuritEase, I spearhead frontend
+              innovations, navigating the evolving JavaScript landscape with a
+              sharp focus on the React ecosystem.
             </p>
           </div>
-        </Card>
-      </div>
-    </>
+        </div>
+
+        <div>
+          <p className="text-md md:text-lg text-gray-700 dark:text-gray-300">
+            I&apos;m deeply invested in{" "}
+            <span className="text-blue-600 dark:text-blue-400 font-semibold">
+              mentoring, building high-performing teams,
+            </span>{" "}
+            and championing inclusive, multicultural workspaces. My career spans
+            across New Zealand, the UK, the USA, and South Africa, fostering a
+            global perspective.
+            <br />
+            <br />
+            Beyond the office, I{" "}
+            <span className="text-green-600 dark:text-green-400 font-semibold">
+              contribute to open source projects, author blog posts,
+            </span>{" "}
+            and engage on StackOverflow. Additionally, I create{" "}
+            <span className="text-purple-600 dark:text-purple-400 font-semibold">
+              educational content
+            </span>{" "}
+            on ReactJS and React Native for YouTube and Udemy. My ambition is to
+            create a lasting, positive impact in the tech world.
+            <br />
+            <br />
+            I&apos;m always open to new challenges and collaborations, eager to
+            connect with fellow tech enthusiasts and innovators. Let’s build
+            groundbreaking digital solutions together!
+          </p>
+        </div>
+      </Card>
+    </div>
   );
 }
