2c7a542a4ce90490fe48c582d32baf68e388addb
Barry Michael Doyle <barry.doyle@universal.one>
2023-11-14 23:49:55 +0200

    minor tweak of nav styles

--- src/components/layout/Navigation/NavItem.tsx
+++ src/components/layout/Navigation/NavItem.tsx
@@ -25,7 +25,7 @@ export function NavItem({
   return (
     <Link
       href={href}
-      className={`px-4 py-2 text-base font-medium text-black dark:text-white hover:text-black dark:hover:text-white border-2 hover:bg-white hover:shadow-md hover:border-primary dark:hover:bg-gray-700 dark:hover:border-primary dark:hover:text-white rounded-md transition
+      className={`px-4 py-2 text-base font-bold text-black dark:text-white hover:text-black dark:hover:text-white border-2 hover:bg-white hover:shadow-md hover:border-primary dark:hover:bg-gray-700 dark:hover:border-primary dark:hover:text-white rounded-md transition
         ${isActive ? "border-black dark:border-white" : "border-transparent"} ${
           className || ""
         }`}
